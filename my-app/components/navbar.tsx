b201b6712f68c234d194fc08aa09779b02d512a6
Jigelu Rani <hemanth.kakarla@gmail.com>
2025-12-28 00:17:52 +0530

    navbar styling

--- my-app/components/navbar.tsx
+++ my-app/components/navbar.tsx
@@ -3,11 +3,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
+      <nav className="navbar">
+          {/* <a>Home</a>
+          <a>Check-in</a>
+          <a>My Goals</a>
+          <a>Settings</a>
+          <span>😊</span> */}
         <Link href="/">Home </Link>
         <Link href="/">Check-in </Link>
         <Link href="/">My Goals </Link>
         <Link href="/">Settings </Link>
         <Link href="/about">About </Link>
+        </nav>
     </main>
   );
 }
